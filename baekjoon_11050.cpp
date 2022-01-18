ab9e55291a8f1a1b5156ac3a7a0699b2882df4b8
SEOKJUN BAEK <tkdldjstm785@gmail.com>
2022-01-18 19:46:21 +0900

    Create baekjoon_11050.cpp

---
+++ baekjoon_11050.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+/*자연수 N과 정수 K가 주어졌을 때 
+이항 계수 (N)
+         (K) 를 구하는 프로그램을 작성하시오.*/
+
+
+int factorial(int);
+
+int main()
+{
+    int n, k, binoCoef;
+    cin>>n>>k;
+    binoCoef = factorial(n)/(factorial(k)*factorial(n-k)); //이항계수: n!/(k!*(n-k)!)
+    cout<<binoCoef;
+}
+
+int factorial(int num)
+{
+    if(num==1||num==0)
+        return 1;
+    else
+        return factorial(num-1)*num;
+}
